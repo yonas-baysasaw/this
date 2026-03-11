3002d778f53f53515d8642e77d9693c5ef820cd2
Yonas Baysasaw Kibret <yonas.baysasaw.kibret@gmail.com>
2026-03-11 07:38:58 +0300

    add this one

--- backend/src/app.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express();
-
-app.use(express.json());
-
-const userRoutes = require('./routes/user.routes');
-app.use('/api/users', userRoutes);
-
-module.exports = app;
