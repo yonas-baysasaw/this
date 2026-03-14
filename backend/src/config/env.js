d7de9a7f2bb4869d7494db982ad665f3a75b34aa
Yonas Baysasaw Kibret <yonas.baysasaw.kibret@gmail.com>
2026-03-14 20:36:09 +0300

    my first server

--- backend/src/config/env.js
+++
@@ -1,8 +0,0 @@
-import dotenv from 'dotenv'
-
-dotenv.config()
-
-export const ENV = {
-    NODE_ENV:process.env.NODE_ENV,
-    PORT:process.env.PORT,
-}
---
+++ server.js
@@ -0,0 +1,7 @@
+const express = require('express');
+
+app = express()
+app.get('/', (req, res)=>{
+  res.send('Hello')
+})
+app.listen(3000)
